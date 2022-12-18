ec24eadc77ce19f119d51bd4881968ac0b7547a9
unknown <arthurribeirobraga@gmail.com>
2022-12-18 08:45:24 -0300

    use-context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext, useState } from "react";
+import "./App.css";
+import { AuthContext } from "./contexts/AuthContext";
 
 function App() {
+  const { isAuthenticated, setIsAuthenticated, user, setUser } = useContext(AuthContext);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuthenticated ? (
+        <div>
+          <h1>Você está logado</h1>
+          <button onClick={() => setIsAuthenticated(false)}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <h1>{user}Você não está logado</h1>
+          <input type="text" onChange={(e)=> {setUser(e.target.value)}}/>
+          <button onClick={() => {setIsAuthenticated(true), setUser(user)}}>Login</button>
+        </div>
+      )}
     </div>
   );
 }
